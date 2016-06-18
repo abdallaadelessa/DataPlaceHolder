904b1f8169854e5b73ac99f74d7657663ef62463
abdalla adel <abdalla.essa92@gmail.com>
2016-06-18 18:56:57 +0200

    clean resourses

--- app/src/main/java/com/abdallaadelessa/android/dataplaceholder/demo/MainActivity.java
+++ app/src/main/java/com/abdallaadelessa/android/dataplaceholder/demo/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final DataPlaceHolder viewById = (DataPlaceHolder) findViewById(R.id.listplaceholder);
+        final DataPlaceHolder viewById = (DataPlaceHolder) findViewById(R.id.dataplaceholder);
         final Handler handler = new Handler();
 
 //        viewById.showActionButton(new Runnable() {
